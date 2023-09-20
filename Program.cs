d289736714c46d3a74c039bc3251d0e14e08e79a
Hellager <86463341+Hellager@users.noreply.github.com>
2023-09-20 23:27:35 +0800

    feat: release v0.0.1

---
+++ Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using CommandLine;
+using QuickAccess;
+using System.Collections;
+using System.Threading;
+
+namespace QuickAccessConsole
+{
+    internal class Program
+    {
+        [Verb("list", HelpText = "List current quick acess or supported languages.")]
+        class ListOptions
+        {
+            [Option('a', "all", Required = false, HelpText = "List both recent files and frequent folders.")]
+            public bool IsListAllAccess { get; set; }
+
+            [Option('r', "recent-files", Required = false, HelpText = "List recent files.")]
+            public bool IsListRecentFiles { get; set; }
+
+            [Option('f', "frequent-folders", Required = false, HelpText = "List frequent folders.")]
+            public bool IsListFrequentFolders { get; set; }
+
+            [Option('u', "ui-culture", Required = false, HelpText = "List system ui culture name.")]
+            public bool IsListUICulture { get; set; }
+        }
+
+        [Verb("remove", HelpText = "Remove items from quick access.")]
+        class RemoveOptions
+        {
+            [Value(0, HelpText = "Targets to remove.")]
+            public IEnumerable<string> RemoveItems { get; set; }
+
+            [Option('m', "menunames", Required = false, HelpText = "Add unsported menu names")]
+            public IEnumerable<string> MenuNames { get; set; }
+        }
+
+        [Verb("check", HelpText = "Check whether in quick access or show quick access or supported language.")]
+        class CheckOptions
+        {
+            [Value(0, HelpText = "Targets to check.")]
+            public IEnumerable<string> Target { get; set; }
+
+            [Option('q', "quick-access", Required = false, HelpText = "Check whether in quick access.")]
+            public bool IsInQuickAccess { get; set; }
+
+            [Option('s', "supported", Required = false, HelpText = "Check whether current system is supported")]
+            public bool IsSupportedSystem { get; set; }
+
+            [Option('m', "menunames", Required = false, HelpText = "Add unsported menu names")]
+            public IEnumerable<string> MenuNames { get; set; }
+        }
+
+        [Verb("empty", HelpText = "Empty quick access.")]
+        class EmptyOptions
+        {
+            [Option('a', "all", Required = false, HelpText = "Empty all quick access items.")]
+            public bool IsEmptyAll { get; set; }
+
+            [Option('r', "recent-files", Required = false, HelpText = "Empty recent files.")]
+            public bool IsEmptyRecentFiles { get; set; }
+
+            [Option('f', "frequent-folders", Required = false, HelpText = "Empty frequent folders.")]
+            public bool IsEmptyFrequentFolders { get; set; }
+        }
+
+        //[Verb("test", HelpText = "Test some feature.")]
+        //class TestOptions
+        //{
+        //    [Option('t', "test", Required = false, HelpText = "Test with option.")]
+        //    public bool IsTestOption { get; set; }
+        //}
+
+        private class OutputData
+        {
+            public ArrayList Data { get; set; }
+            public string Type { get; set; }
+            public ArrayList Debug { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            QuickAccessHandler _handler = new QuickAccessHandler();
+
+            Parser.Default.ParseArguments<ListOptions, RemoveOptions, CheckOptions, EmptyOptions/*, TestOptions*/>(args)
+                .MapResult(
+                    (ListOptions _option) => HandleListOptions(_option, _handler),
+                    (RemoveOptions _option) => HandleRemoveOptions(args, _option, _handler),
+                    (CheckOptions _option) => HandleCheckOptions(args, _option, _handler),
+                    (EmptyOptions _option) => HandleEmptyOptions(_option, _handler),
+                    //(TestOptions _option) => HandleTestOptions(_option, _handler),
+                    error => -1
+                );
+        }
+
+        private static string BuildOutputData(ArrayList input, string type, ArrayList debug)
+        {
+            var output = new OutputData
+            {
+                Data = input,
+                Type = type,
+                Debug = debug
+            };
+
+            return JsonConvert.SerializeObject(output);
+        }
+
+        private static int HandleListOptions(ListOptions options, QuickAccessHandler handler)
+        {
+            List<string> res = new List<string>() { };
+            string uiCultureName = "";
+
+            if (options.IsListRecentFiles)
+            {
+                res = handler.GetRecentFilesList();
+            }
+            else if (options.IsListFrequentFolders)
+            {
+                res = handler.GetFrequentFoldersList();
+            }
+            else if (options.IsListRecentFiles)
+            {
+                res = handler.GetQuickAccessList();
+            }
+            else if (options.IsListUICulture)
+            {
+                uiCultureName = handler.GetSystemUICultureCode();
+            }
+
+            ArrayList _data = new ArrayList { };
+            foreach (var item in res)
+            {
+                _data.Add(item);
+            }
+
+            if (uiCultureName != "")
+            {
+                _data.Add(uiCultureName);
+            }
+
+            ArrayList _debug = new ArrayList { };
+            Console.WriteLine(BuildOutputData(_data, "list", _debug));
+
+            return 0;
+        }
+
+        private static int HandleRemoveOptions(IEnumerable<string> args, RemoveOptions options, QuickAccessHandler handler)
+        {
+            ArrayList _data = new ArrayList { };
+            ArrayList _debug = new ArrayList { };
+
+            List<string> menuNameList = options.MenuNames.ToList<string>();
+            List<string> removeList = options.RemoveItems.ToList<string>();
+
+            if (menuNameList.Count > 0)
+            {
+                foreach (var name in menuNameList)
+                {
+                    handler.AddQuickAccessMenuName(name);
+                }
+            }
+
+            CancellationTokenSource taskCancelToken = new CancellationTokenSource();
+            var task = Task.Factory.StartNew(() =>
+            {
+                handler.RemoveFromQuickAccess(removeList);
+            });
+            if (!task.Wait(10 * 1000, taskCancelToken.Token))
+            {
+                _debug.Add("remove timeout");
+                Console.Error.WriteLine("Remove function timeout");
+
+                Console.WriteLine(BuildOutputData(_data, "remove", _debug));
+                return -1;
+            }
+
+            foreach (var item in removeList)
+            {
+                bool res = handler.IsInQuickAccess(item);
+
+                _data.Add(res);
+            }
+
+            Console.WriteLine(BuildOutputData(_data, "remove", _debug));
+
+            return 0;
+        }
+
+        private static int HandleCheckOptions(IEnumerable<string> args, CheckOptions options, QuickAccessHandler handler)
+        {
+            ArrayList res = new ArrayList { };
+
+            List<string> menuNameList = options.MenuNames.ToList<string>();
+
+            if (menuNameList.Count > 0)
+            {
+                foreach (var name in menuNameList)
+                {
+                    handler.AddQuickAccessMenuName(name);
+                }
+            }
+
+            if (options.IsInQuickAccess)
+            {
+                foreach (var item in options.Target)
+                {
+                    bool isInQuickAccess = handler.IsInQuickAccess(item);
+                    res.Add(isInQuickAccess);
+                }
+            }
+            else if (options.IsSupportedSystem)
+            {
+                bool isSupported = handler.IsSupportedSystem();
+
+                res.Add(isSupported);
+            }
+
+            ArrayList _debug = new ArrayList { };
+            Console.WriteLine(BuildOutputData(res, "check", _debug));
+
+            return 0;
+        }
+
+        private static int HandleEmptyOptions(EmptyOptions options, QuickAccessHandler handler)
+        {
+            if (options.IsEmptyAll)
+            {
+                handler.EmptyQuickAccess();
+            }
+            else if (options.IsEmptyRecentFiles)
+            {
+                handler.EmptyRecentFiles();
+            }
+            else if (options.IsEmptyFrequentFolders)
+            {
+                handler.EmptyFrequentFolders();
+            }
+
+            return 0;
+        }
+
+        //private static int HandleTestOptions(TestOptions options)
+        //{
+        //    CultureInfo ci = CultureInfo.CurrentCulture;
+        //    Console.WriteLine("Current Language Info: {0}", ci.Name);
+
+        //    ArrayList CurSupportLanguageList = new ArrayList(_handler.GetSupportLanguages());
+        //    if (CurSupportLanguageList.Contains(ci.Name))
+        //    {
+        //        Console.WriteLine("Support cur system lang");
+        //    } else
+        //    {
+        //        Console.WriteLine("Unsupported cur system lang");
+        //    }
+
+        //    return 0;
+        //}
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# quick-access-console
-Console application for handling windows quick access, specific for clean-recent
+# QuickAccessConsole
+
+QuickAccessConsole is a c# console application based on .net framework for handling windows quick access.
+
+This is customized for [clean-recent](https://github.com/Hellager/clean-recent).
+
+## Installation
+
+### 1. From release page
+
+Check [release page](https://github.com/Hellager/clean-recent/releases) and download `QuickAccessConsole.exe`.
+
+### 2. Build it yourself
+
+```c
+$ git clone https://github.com/Hellager/quick-access-console
+$ msbuild -t:restore QuickAccessConsole.sln
+$ msbuild QuickAccessConsole.sln /p:platform="Any CPU" /p:configuration="Debug"
+```
+
+## Usage
+
+```shell
+$ .\QuickAccessConsole.exe
+
+QuickAccessConsole 0.0.0.1
+Copyright ©  2023 Steins.
+
+ERROR(S):
+  No verb selected.
+
+  list       List current quick acess or supported languages.
+
+  remove     Remove items from quick access.
+
+  check      Check whether in quick access or show quick access or supported language.
+
+  empty      Empty quick access.
+
+  help       Display more information on a specific command.
+
+  version    Display version information.
+```
+
+
+
+## Verbs & Options
+
+### list
+
+```shell
+  -a, --all                     List both recent files and frequent folders.
+
+  -r, --recent-files            List recent files.
+
+  -f, --frequent-folders        List frequent folders.
+  
+  -u, --ui-culture              List system ui culture name.
+```
+
+### remove
+
+```shell
+  value pos. 0                  Targets to remove.
+  
+  -m, --menunames               Add unsported menu names
+```
+
+### check
+
+```shell
+  value pos. 0                    Targets to check.
+  
+  -q, --quick-access              Check whether in quick access.
+
+  -s, --supported                 Check whether current system is supported
+
+  -m, --menunames                 Add unsported menu names
+```
+
+### empty
+
+```shell
+  -a, --all                 Empty all quick access items.
+
+  -r, --recent-files        Empty recent files.
+
+  -f, --frequent-folders    Empty frequent folders.
+```
+
+
+
+## Contributing
+
+Pull requests are welcome. 
+
+It's better to make your own console applcation since this is customized.
+
+
+
+## License
+
+[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)
